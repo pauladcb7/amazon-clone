3117c914257ed63cffd1c81487b6c70077c1c85c
pauladcb7 <62583651+pauladcb7@users.noreply.github.com>
2020-09-09 19:13:26 -0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAtHiY1F1ve__nBF3U4pU3devL4_0ETd7M",
-    authDomain: "clone-e75af.firebaseapp.com",
-    databaseURL: "https://clone-e75af.firebaseio.com",
-    projectId: "clone-e75af",
-    storageBucket: "clone-e75af.appspot.com",
+    apiKey: "************",
+    authDomain: "********.firebaseapp.com",
+    databaseURL: "https://*********.firebaseio.com",
+    projectId: "**********",
+    storageBucket: "*********.appspot.com",
     messagingSenderId: "637698537946",
-    appId: "1:637698537946:web:e22c5719137ccfd26e68e3",
-    measurementId: "G-LELRN0EP9S"
+    appId: "*:*********:web:**************",
+    measurementId: "*-*********"
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
